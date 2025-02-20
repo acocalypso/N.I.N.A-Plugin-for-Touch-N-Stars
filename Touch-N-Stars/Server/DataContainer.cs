3b4b7bd48f4fcd90a6e071a57d20f568d0c5953d
JohannesWorks <121811969+JohannesWorks@users.noreply.github.com>
2025-02-20 07:32:57 +0100

    fix load af graph

--- Touch-N-Stars/Server/DataContainer.cs
+++ Touch-N-Stars/Server/DataContainer.cs
@@ -1,7 +1,11 @@
+using System;
+using TouchNStars.Server;
+
 internal static class DataContainer {
     internal static object lockObj = new object();
     internal static bool afRun = false;
     internal static bool afError = false;
     internal static string afErrorText = string.Empty;
     internal static bool newAfGraph = false;
+    internal static DateTime lastAfTimestamp = DateTime.MinValue;
 }
